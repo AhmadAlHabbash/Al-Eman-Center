23a8423c929f9eaee7f820fbd1277cc4940abc6f
AhmadAlHabbash <ahmado-1994@hotmail.com>
2018-02-26 15:50:49 +0200

    Add User class

---
+++ Tahfiz/classes/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tahfiz.classes
+{
+    public class User
+    {
+        public string ID { get; }
+        public string username { get; }
+
+        public User(string ID, string username)
+        {
+            this.ID = ID;
+            this.username = username;
+        }
+    }
+}
